d70ac692cd270092a09d93df31088be82c5786ae
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-19 21:52:16 +0900

    update: use facility and shake dice

--- src/player.py
+++ src/player.py
@@ -1,4 +1,8 @@
 from src.facility import Facility, FacilityType
+from src.landmark import Landmark, LandmarkType
+from src.facility_factory import FacilityFactory
+
+import random
 
 '''
 所持金
@@ -7,6 +11,7 @@ from src.facility import Facility, FacilityType
 '''
 class Player:
     def __init__(self, num):
+        self.ff = FacilityFactory()
         self.num = num
         # 初期所持金
         self.money = 3
@@ -18,13 +23,63 @@ class Player:
         self.init_facility()
         # ランドマーク
         self.landmark = []
+        self.landmark_type = []
 
-        self.print_having_facility()
-        self.print_having_money()
+        # self.print_having_facility()
+        # self.print_having_money()
 
     def init_facility(self):
-        self.blue_facility.append(Facility(FacilityType.WheatField))
-        self.green_facility.append(Facility(FacilityType.Bakery))
+        self.blue_facility.append(self.ff.create_facility(FacilityType.WheatField))
+        self.green_facility.append(self.ff.create_facility(FacilityType.Bakery))
+
+    def play_red_facility(self, dice):
+        money = 0
+        for f in self.red_facility:
+            if dice in f.get_pip():
+                money += f.play()
+
+        return money
+
+    def play_blue_facility(self, dice):
+        money = 0
+        for f in self.blue_facility:
+            if dice in f.get_pip():
+                self.money += f.play()
+
+    def play_green_facility(self, dice):
+        money = 0
+        for f in self.green_facility:
+            if dice in f.get_pip():
+                self.money += f.play()
+
+    def play_purple_facility(self, dice):
+        pass
+
+    def shake_dice(self):
+        dice_num = 1
+        if LandmarkType.TrainStation in self.landmark_type:
+            dice_num = int(input("How many dice do you shake?(1 or 2) > "))
+            while(True):
+                if not dice_num == 1 and not dice_num == 2:
+                    print("Please input 1 or 2")
+                    dice_num = int(input("How many dice do you shake?(1 or 2) > "))
+                else:
+                    break
+        dice = random.randint(1, 6)
+        if dice_num == 2:
+            dice += random.randint(1, 6)
+        return dice
+
+    def add_money(self, money):
+        self.money += money
+
+    def sub_money(self, money):
+        if money > self.money:
+            self.money = 0
+            return self.money
+        else:
+            self.money -= money
+            return money
 
     def print_having_money(self):
         print("Player{}".format(self.num))
