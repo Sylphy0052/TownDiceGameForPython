afe8cb00006bd3c5a2abfdc3173de1256b2ff323
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-19 22:05:24 +0900

    update: add facility class

--- src/facility.py
+++ src/facility.py
@@ -54,9 +54,79 @@ class WheatField(Facility):
     def play(self):
         return 1
 
+class Ranch(Facility):
+    def __init__(self):
+        super().__init__("Ranch", [2], 1, Color.Blue)
+
+    def play(self):
+        return 1
+
 class Bakery(Facility):
     def __init__(self):
         super().__init__("Bakery", [2, 3], 1, Color.Green)
 
     def play(self):
         return 1
+
+class Cafe(Facility):
+    def __init__(self):
+        super().__init__("Cafe", [3], 1, Color.Red)
+
+    def play(self):
+        return 1
+
+class ConvenienceStore(Facility):
+    def __init__(self):
+        super().__init__("Convenience Store", [4], 2, Color.Green)
+
+    def play(self):
+        return 3
+
+class Forest(Facility):
+    def __init__(self):
+        super().__init__("Forest", [5], 3, Color.Blue)
+
+    def play(self):
+        return 1
+
+class CheeseFactory(Facility):
+    def __init__(self):
+        super().__init__("Cheese Factory", [7], 5, Color.Green)
+
+    def play(self):
+        return 3
+
+class FurnitureFactory(Facility):
+    def __init__(self):
+        super().__init__("Furniture Factory", [8], 3, Color.Green)
+
+    def play(self):
+        return 3
+
+class Mine(Facility):
+    def __init__(self):
+        super().__init__("Mine", [9], 6, Color.Blue)
+
+    def play(self):
+        return 5
+
+class FamilyRestaurant(Facility):
+    def __init__(self):
+        super().__init__("Family Restaurant", [9, 10], 3, Color.Red)
+
+    def play(self):
+        return 2
+
+class AppleOrchard(Facility):
+    def __init__(self):
+        super().__init__("Apple Orchard", [10], 3, Color.Blue)
+
+    def play(self):
+        return 3
+
+class FruitAndVegetableMarket(Facility):
+    def __init__(self):
+        super().__init__("Fruit and Vegetable Market", [11, 12], 2, Color.Green)
+
+    def play(self):
+        return 2
